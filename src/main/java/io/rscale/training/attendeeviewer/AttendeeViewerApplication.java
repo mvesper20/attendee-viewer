f5b05e0f4f414fae5d975d70a883aecfca21b8d2
Miles Vesper <mvesper20@gmail.com>
2018-08-23 16:02:10 -0600

    Move config and add hystrix support

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
@@ -1,6 +1,8 @@
 package io.rscale.training.attendeeviewer;
 
 import org.springframework.boot.SpringApplication;
+
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class AttendeeViewerApplication {
 
 	public static void main(String[] args) {
